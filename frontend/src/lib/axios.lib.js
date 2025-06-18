9069f98bbef011de5a77014fca71f485b4a097af
SanmithD <sanmithdevadiga7@gmail.com>
2025-06-18 09:23:22 +0530

    changes in the lib file

--- frontend/src/lib/axios.lib.js
+++ frontend/src/lib/axios.lib.js
@@ -5,6 +5,6 @@ const isLocalhost = window.location.hostname === 'localhost';
 export const axiosInstance = axios.create({
   baseURL: isLocalhost
     ? 'http://localhost:5001/api'
-    : 'https://voicely-ed4j.onrender.com/app',
+    : 'https://voicely-ed4j.onrender.com/api',
   withCredentials: true,
 });
