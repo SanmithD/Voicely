8ef7f3dc2ec3d03a1b503599f42c5b94c62b120e
SanmithD <sanmithdevadiga7@gmail.com>
2025-06-18 08:58:30 +0530

    frontend hosting

--- frontend/src/lib/axios.lib.js
+++ frontend/src/lib/axios.lib.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
+const isLocalhost = window.location.hostname === 'localhost';
+
 export const axiosInstance = axios.create({
-    baseURL: `http://localhost:5001/api`,
-    withCredentials: true
+  baseURL: isLocalhost
+    ? 'http://localhost:5001/api'
+    : 'https://voicely-ed4j.onrender.com/app',
+  withCredentials: true,
 });
