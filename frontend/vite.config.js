df7a1f39e03c3d57c65d3ee6f73dde3ba75a1977
SanmithD <sanmithdevadiga7@gmail.com>
2025-06-19 20:13:49 +0530

    changes made

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { qrcode } from 'vite-plugin-qrcode';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    qrcode(),
     tailwindcss()
   ],
 })
